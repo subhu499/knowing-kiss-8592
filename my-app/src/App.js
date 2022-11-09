aea34efcc074cae21da19d2081f64cdd446c123d
subhu499 <subhu499@gmail.com>
2022-11-09 19:10:37 +0530

    NavBar

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      
+            <Navbar/>
+           
+ 
+    </div>
+  );
+}
+
+export default App;
